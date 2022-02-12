596c96c40ce55c408e234625bc5e1c0f2cad41ae
angelrosarioco <arosario87@live.com>
2022-02-12 03:26:09 -0500

    Initial Commit

---
+++ The Bartender/The_BartenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_BartenderApp.swift
+//  The Bartender
+//
+//  Created by Daniela Rosario on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct The_BartenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
